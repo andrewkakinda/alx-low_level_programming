c1700a9d5c8552c698736a8dfdf70c3bed7b0071
andrewkakinda <kakindaandrewk@gmail.com>
2022-07-03 10:56:45 +0300

    initial commit of 101 file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include	<stdio.h>
+/**
+ * main - entry point
+ * Return: always 0
+ */
+int	main(void)
+{
+	fprintf(stderr, "%s", "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n");
+	return	(0);
+}
