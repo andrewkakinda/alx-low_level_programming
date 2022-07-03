a7c9f1419b5cef374f65ae101f0eb6cd64c9750f
andrewkakinda <kakindaandrewk@gmail.com>
2022-07-03 11:19:31 +0300

    changes to 101 code

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include	<stdio.h>
+#include	<unistd.h>
 /**
  * main - entry point
- * Return: always 0
+ * Return: always 1
  */
 int	main(void)
 {
-	fprintf(stderr, "%s", "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n");
-	return	(0);
+	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
+	return	(1);
 }
