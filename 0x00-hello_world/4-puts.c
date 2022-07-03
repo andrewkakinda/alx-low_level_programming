48af08500d38e5551e7d472dbf29567dda29caba
andrewkakinda <kakindaandrewk@gmail.com>
2022-07-03 09:19:30 +0300

    edits on file 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,9 @@
-#include <stdio.h>
-
+#include	<stdio.h>
 /**
-*main - Entry point
-*
-*Return: always 0
-*/
-
-int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle\n");
-return (0);
+ * main-entry point
+ * Return: Always 0 (success)
+ */
+int	main(void)
+{	printf("Programming is like building a multilingual puzzle\n");
+	return	(0);
 }
