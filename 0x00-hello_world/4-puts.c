dfb073be8309c192658b18e2fdb9b42504620505
andrewkakinda <kakindaandrewk@gmail.com>
2022-07-03 09:28:07 +0300

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,6 @@
  * Return: Always 0 (success)
  */
 int	main(void)
-{	printf("Programming is like building a multilingual puzzle\n");
+{	puts("\"Programming is like building a multilingual puzzle");
 	return	(0);
 }
