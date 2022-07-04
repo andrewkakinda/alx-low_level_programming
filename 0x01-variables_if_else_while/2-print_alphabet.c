b1d0fedb03c00bf30c63d1b2be747aaf02002c4a
andrewkakinda <kakindaandrewk@gmail.com>
2022-07-04 16:07:12 +0300

    initial commit of alphabet file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include	<stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always return 0
+ *
+ */
+int	main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+
+	return	(0);
+}
