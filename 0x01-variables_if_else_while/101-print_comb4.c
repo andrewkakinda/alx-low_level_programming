562cdaf175432eae096dc1eef888a27170ef521f
andrewkakinda <kakindaandrewk@gmail.com>
2022-07-05 00:50:45 +0300

    initial commit of file 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include	<stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ *
+ */
+int	main(void)
+{
+	int i;
+	int t;
+	int h; /*hundreds */
+
+	for (h = '0'; h <= '9'; h++)
+	{
+		for (i = (t + 1); i <= '9'; i++)
+		{
+			putchar(h);
+			putchar(t);
+			putchar(i);
+			if (h != '7' || t != '8' || i != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
