479d57c8ba0420160fa4d2354565ae1092c509c6
andrewkakinda <kakindaandrewk@gmail.com>
2022-07-06 01:51:13 +0300

    5 sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include	"main.h"
+
+/**
+ * print_sign - prints a sign of a number
+ *
+ * @n: is the number to be compared
+ *
+ * Return: 1 if greater than 0, 0 if equal 0, -1 if less than 0
+ *
+ */
+int	print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else	if (n == 0)
+	{
+		return (0);
+		_putchar('0');
+	}
+	else	if (n < 0)
+	{
+		return (-1);
+		_putchar ('-');
+	}
+}
