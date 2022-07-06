4ce8c0ad67361cf674ae904230c87afdf9d0cc7f
andrewkakinda <kakindaandrewk@gmail.com>
2022-07-06 04:23:21 +0300

    edits

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,33 +16,32 @@ void	print_times_table(int n)
 				prod = row * col;
 				if (col == 0)
 				{
-					_putchar('0' + prod);
-				}
-				else
+					_putchar('0');
+				} else	if (prod <= 9)
 				{
 					_putchar(',');
 					_putchar(' ');
-					if (prod <= 9)
-					{
-						_putchar(' ');
-						_putchar(' ');
-						_putchar('0' + prod);
-					}
-					else	if (prod > 9 && prod < 100)
-					{
-						_putchar(' ');
-						_putchar('0' + (prod / 10));
-						_putchar('0' + (prod % 10));
-					}
-					else	if (prod >= 100)
-					{
-						_putchar('0' + (prod / 100));
-						_putchar('0' + (prod / 100) % 10);
-						_putchar('0' + (prod % 10));
-					}
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + prod);
+				} else	if (prod > 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar('0' + (prod / 100));
+					_putchar('0' + (prod / 100) % 10);
+					_putchar('0' + (prod % 10));
+				} else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + (prod / 10));
+					_putchar('0' + (prod % 10));
 				}
 			}
 			_putchar('\n');
 		}
 	}
 }
+
